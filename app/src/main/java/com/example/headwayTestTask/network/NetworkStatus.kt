75c139cf739c3614e5e16109d2adf92fec8fdd6c
Sk1droWWW <Skidrow132879@gmail.com>
2021-08-20 12:02:16 +0300

    fix: update network behaviour

---
+++ app/src/main/java/com/example/headwayTestTask/network/NetworkStatus.kt
@@ -0,0 +1,14 @@
+package com.example.headwayTestTask.network
+
+data class NetworkStatus(
+    var status: Int,
+    var showMessage: Boolean = false,
+    var message: String = ""
+) {
+    companion object {
+        const val SUCCESS = 2
+        const val NOT_FOUND = 3
+        const val ERROR = -1
+        const val LOADING = 1
+    }
+}
--- app/src/main/java/com/example/headwayTestTask/network/service/GitHubApiService.kt
+++ app/src/main/java/com/example/headwayTestTask/network/service/GitHubApiService.kt
@@ -1,6 +1,7 @@
 package com.example.headwayTestTask.network.service
 
-import com.example.headwayTestTask.network.model.GitHubSearchModel
+import com.example.headwayTestTask.model.GitHubSearchItemModel
+import com.example.headwayTestTask.model.GitHubSearchModel
 import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -11,11 +12,14 @@ import retrofit2.http.Query
 interface GithubApiService {
 
     @GET("search/repositories")
-    fun searchGitHubRepo(
+    fun getGitHubRepos(
         @Query("q")         searchParam : String,
+        @Query("since")     userId: Int,
         @Query("sort")      sort : String = "stars",
         @Query("order")     order : String = "desc",
-        @Query("per_page")  perPage : Int = 15) : Observable<GitHubSearchModel>
+        @Query("per_page")  perPage : Int = 15,
+        @Query("page")      page: Int = 1
+    ) : Observable<GitHubSearchModel>
 
     /**
      * Companion object to create the GithubApiService
@@ -34,8 +38,10 @@ interface GithubApiService {
 }
 
 class SearchRepository(private val apiService: GithubApiService) {
-    fun searchGitHubRepo(repoName: String): Observable<GitHubSearchModel> {
-        return apiService.searchGitHubRepo(searchParam = repoName)
+    fun search(query: String, page: Int): Observable<List<GitHubSearchItemModel>> {
+        return apiService.getGitHubRepos(query, page).flatMap {
+            Observable.just(it.items)
+        }
     }
 }
 
